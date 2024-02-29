2f9b362f4ad4fc6092e001d68e176740d3a093a3
vml4 <vimal.kthomas@batonsystems>
2024-02-29 12:51:53 +0530

    New upadated

--- book.sh
+++ book.sh
@@ -1,3 +1,5 @@
 read -p "Enter the link " link
 
+echo -e "\n"
+
 wget -O downloaded_book.txt $link --no-check-certificate  && cat downloaded_book.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr ' ' '\n' | grep -v '^$' | sort | uniq -c | sort -nr | head -n 10
